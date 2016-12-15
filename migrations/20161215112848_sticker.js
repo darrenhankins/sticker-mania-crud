b056bd3a5e7cf23ca78e0681c2c385a5278ff58a
Darren Hankins <hankins.darren@gmail.com>
2016-12-15 11:34:20 -0700

    created sticker table

---
+++ migrations/20161215112848_sticker.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('sticker', table => {
+    table.increments();
+    table.text('description').notNullable();
+    table.text('image_url').notNullable();
+    table.integer('quantity').notNullable().defaultTo(0);
+    table.text('size');
+    table.integer('user_id').references('user.id').unsigned().onDelete('cascade');
+
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('sticker');
+};
